ffa973c5a19b033ffcb319a4529d54aaf3c888c0
KathikelRG <kathi.kellenberger@outlook.com>
2024-05-10 08:48:58 -0500

    Improved spGetTopCustomers

---
+++ migrations/U008_20240510134700__UNDO-Improved spGetTopCustomers.sql
@@ -0,0 +1,28 @@
+﻿SET NUMERIC_ROUNDABORT OFF
+GO
+SET ANSI_PADDING, ANSI_WARNINGS, CONCAT_NULL_YIELDS_NULL, ARITHABORT, QUOTED_IDENTIFIER, ANSI_NULLS ON
+GO
+PRINT N'Altering [dbo].[spGetTopCustomers]'
+GO
+ALTER   PROC [dbo].[spGetTopCustomers] AS;
+    --Use window function to rank customers sorted by most invoices
+    --Return their ids and invoice count
+
+    WITH                                                  rankings
+                                                          AS ( SELECT   i.customer_id ,
+                                                                        COUNT (
+                                                                            *)
+                                                                        AS
+                                                                        invoices_count ,
+                                                                        ranking = ROW_NUMBER () OVER ( ORDER BY COUNT (
+                                                                                                                    *) DESC )
+                                                               FROM     dbo.INVOICE i
+                                                               GROUP BY i.customer_id )
+    SELECT   TOP ( 100 ) C.customer_id, r.invoices_count 
+    FROM     dbo.CUSTOMER c
+             LEFT JOIN rankings r ON c.customer_id = r.customer_id
+    WHERE r.ranking is not null
+    ORDER BY r.ranking ASC ,
+             c.customer_id;
+GO
+
